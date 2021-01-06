37ec387fac5ab98bf10a1790ad3a4c823f507744
sertachak <sertaconurhakbilen@gmail.com>
2021-01-06 19:53:18 +0300

    Error Boundries

---
+++ projectWork/src/ErrorBoundry/ErrorBoundry.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react'
+
+class ErrorBoundry extends Component {
+
+    state = {
+        hasError : false,
+        errorMessage: ''
+    }
+
+    componentDidCatch(error, errorInfo) {
+        this.setState({
+            hasError: true,
+            errorMessage: error
+        })
+    }
+
+
+    render() {
+            if( this.state.hasError) {
+               return (<h1>{this.state.errorMessage}</h1>);
+            } else {
+                return ( this.props.children );
+            }
+    }
+}
+
+export default ErrorBoundry;
