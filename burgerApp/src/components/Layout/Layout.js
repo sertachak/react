97d3bf2da0ee96c316694edb4279809f1dad4941
sertachak <sertaconurhakbilen@gmail.com>
2021-01-14 18:04:09 +0300

    Ingredients

--- burgerApp/src/components/Layout/Layout.js
+++ burgerApp/src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import Auxilary from '../../hoc/Auxilary'
+import classes from './Layout.module.css'
 
 const layout = (props) => {
     return (
     <Auxilary>
         <div>SideDrawer, toolbar, backdrop</div>
-        <main>
+        <main className={classes.Component}>
             {props.children}
         </main>
     </Auxilary>
