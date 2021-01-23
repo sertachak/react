fc4b48809db7d15d9a6272abc7be0b9327002119
sertachak <sertaconurhakbilen@gmail.com>
2021-01-23 18:50:10 +0300

    Mobile device problem.

--- burgerApp/src/components/UI/Modal/Modal.js
+++ burgerApp/src/components/UI/Modal/Modal.js
@@ -10,7 +10,7 @@ const modal = (props) => {
             <Backdrop show={props.show} clicked={props.closeBackdrop}/>
             <div className={classes.Modal}
                  style={{
-                     transform: props.show ? 'translateY(0)' : 'translateY(-100)',
+                     transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
                      opacity: props.show ? 1 : 0
                  }
                  }>
