10f1f217bca747d715c474ad7b7116b8b2d1f1c0
sertachak <sertaconurhakbilen@gmail.com>
2021-02-28 12:46:08 +0300

    Checkout Page

--- burgerApp/src/App.js
+++ burgerApp/src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import Layout from './components/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
-
+import Checkout from './containers/Checkout/Checkout'
 
 class App extends Component {
 
@@ -10,6 +10,7 @@ class App extends Component {
             <div>
                 <Layout>
                     <BurgerBuilder/>
+                    <Checkout/>
                 </Layout>
             </div>
         );
