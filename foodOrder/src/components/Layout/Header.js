c6bc8533cdc01fbc1da11d38726cc4bf1381e420
sertachak <sertaconurhakbilen@gmail.com>
2021-05-16 11:39:19 +0300

    foodOrder -> Header and headerButton

---
+++ foodOrder/src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import meals from '../../assets/meals.jpg'
+import classes from './Header.module.css'
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+    return(
+        <React.Fragment>
+            <header className={classes.header}>
+                <h1>React Meals</h1>
+                <HeaderCartButton>Cart</HeaderCartButton>
+            </header>
+            <div className={classes['main-image']}>
+                <img src={meals} alt="A table of food"/>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default Header;
